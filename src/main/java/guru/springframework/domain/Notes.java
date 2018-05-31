e8ab77edb7a5bfdf3cf3f45976b7e51749eda1fa
Yunfei <philipguo1992@gmail.com>
2018-05-31 16:34:11 -0300

    fix bug

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -1,10 +1,12 @@
 package guru.springframework.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
@@ -19,43 +21,5 @@ public class Notes {
     private String recipeNotes;
 
     public Notes() {
-    }
-
-    public boolean equals(Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Notes)) return false;
-        final Notes other = (Notes) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
-        final Object this$recipe = this.getRecipe();
-        final Object other$recipe = other.getRecipe();
-        if (this$recipe == null ? other$recipe != null : !this$recipe.equals(other$recipe)) return false;
-        final Object this$recipeNotes = this.getRecipeNotes();
-        final Object other$recipeNotes = other.getRecipeNotes();
-        if (this$recipeNotes == null ? other$recipeNotes != null : !this$recipeNotes.equals(other$recipeNotes))
-            return false;
-        return true;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        final Object $recipe = this.getRecipe();
-        result = result * PRIME + ($recipe == null ? 43 : $recipe.hashCode());
-        final Object $recipeNotes = this.getRecipeNotes();
-        result = result * PRIME + ($recipeNotes == null ? 43 : $recipeNotes.hashCode());
-        return result;
-    }
-
-    protected boolean canEqual(Object other) {
-        return other instanceof Notes;
-    }
-
-    public String toString() {
-        return "Notes(id=" + this.getId() + ", recipe=" + this.getRecipe() + ", recipeNotes=" + this.getRecipeNotes() + ")";
     }
 }
